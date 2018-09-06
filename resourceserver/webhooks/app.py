e94bd7af8a5f0421b01cf0b845b6b65240138cdc
Leon Chou <leon.chou@nyu.edu>
2018-09-06 12:06:51 -0700

    small fixes

--- resourceserver/webhooks/app.py
+++ resourceserver/webhooks/app.py
@@ -1,12 +1,11 @@
 from os import environ as env
 
 import hmac
-from threading import Thread
+import subprocess
 
 from flask import Flask, render_template, request, abort
 
-DATA_DIR = env.get('DATA_DIR') or '/mnt/data'
-RESUME_SECRET_HASH = sha1(env.get('RESUME_SECRET_TOKEN'))
+DATA_DIR = '/mnt/data'
 
 app = Flask(__name__)
 
@@ -15,20 +14,26 @@ def index():
   return render_template('index.html')
 
 
-@app.route('/resume', methods='POST')
+@app.route('/resume', methods=['POST'])
 def resume_hook():
 
   def verify_signature(payload, signature):
-    h = hmac.new(env.get('RESUME_SECRET_TOKEN'), payload, 'sha1')
+    h = hmac.new(env.get('RESUME_SECRET_TOKEN').encode('utf-8'), payload, 'sha1')
     recomputed = 'sha1=' + h.hexdigest() # compute hash
     return hmac.compare_digest(recomputed, signature)
 
+  # NOTE: request.data is only accessible with request_type: application/json
   if not verify_signature(request.data, request.headers['X-Hub-Signature']):
     return abort(400)
   
   # TODO: git updates here.
 
+  git_pull(DATA_DIR + '/resume')
+
   return 'OK'
+
+def git_pull(location):
+  subprocess.run(['cd', location, '&&', 'git', 'pull'])
 
 if __name__ == '__main__':
   app.run('0.0.0.0', port=env.get('PORT') or 8000)
